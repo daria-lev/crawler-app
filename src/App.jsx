c01f33f1a381aace7bc44add6eb1cc3be3a6c93f
daria <d11kova@hotmail.com>
2025-02-06 00:03:35 -0800

    functioning size change

--- src/App.jsx
+++ src/App.jsx
@@ -22,46 +22,15 @@ function Tile() {
   );
 }
 
-class BoardMaybe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.reset();
-  }
-
-  reset() {
-    this.state = {
-      boardSize: [5,7]
-    };
-  }
-
-  createBoard() {
-    const board = []
-    for (var i = 0; i < boardSize[0]; i++) {
-      const row = []
-      for (var j = 0; j < boardSize[1]; j++) {
-        row.push(<Tile></Tile>) 
-      }
-      board.push(<div>{row}</div>)
-    }
-    return board
-  }
-
-  render() {
-    return(
-      <div>{createBoard()}</div>
-    ); 
-  }
-}
-
-function Board() {
-  const [boardSize, setSize] = useState([5,7]) //height, width
-  const canEdit = true
+function Board(props) {
+  //const [boardSize, setSize] = useState([props.height, props.width]) //height, width
+  const [canEdit, setEditable] = useState(true)
 
   function createBoard() {
     const board = []
-    for (var i = 0; i < boardSize[0]; i++) {
+    for (var i = 0; i < props.height; i++) {
       const row = []
-      for (var j = 0; j < boardSize[1]; j++) {
+      for (var j = 0; j < props.width; j++) {
         row.push(<Tile></Tile>) 
       }
       board.push(<div>{row}</div>)
@@ -77,39 +46,52 @@ function Board() {
 
 function App() {
   //const items = ["pear", "banana"]
-  const [height, setHeight] = useState("")
-  const [width, setWidth] = useState("")
+  const [height, setHeight] = useState(5)
+  const [width, setWidth] = useState(7)
+  const [canEdit, setEditable] = useState(true)
+  let widthVal = width
+  let heightVal = height
   
 
   function handleSizeClick() {
-    alert("change board size");
-  }
-
-  function onWidthChange(){
-    setWidth()
-  }
-
-  function onHeightChange() {
-    
-  }
-
-  function handleTileClick() {
-    if (canEdit) {
-
+    //alert(widthVal + " " + heightVal);
+    if (widthVal > 0 && heightVal > 0 && heightVal < 10 && widthVal < 20) {
+      setHeight(heightVal)
+      setWidth(widthVal)
+    } else {
+      alert("invalid")
     }
-  } 
+  }
+
+  function onWidthChange(e){
+    let val = parseInt(e)
+    //alert(val)
+    widthVal = val;
+  }
+
+  function onHeightChange(e) {
+    let val = parseInt(e)
+    //alert(val)
+    heightVal = val;
+  }
+
+  function handleStartClick() {
+
+  }
 
   return (
     <>
       <div className="p-3">
         <label className='inputLabel'>Width:</label>
-        <input type="number" onChange={onWidthChange}></input> 
+        <input type="number" min="1" step="1" onChange={e=>onWidthChange(e.target.value)} 
+          defaultValue={width}></input> 
         <label className='inputLabel'>Height:</label>
-        <input type="number" onChange={onHeightChange}></input>
+        <input type="number" min="1" step="1" onChange={e=>onHeightChange(e.target.value)} 
+          defaultValue={height}></input>
       </div>
       <button style={{margin: '10px'}} onClick={handleSizeClick}>Update Board Size</button>
-      <div style={{margin: '20px'}}><Board></Board></div>
-      
+      <div style={{margin: '20px'}}><Board height={height} width={width}></Board></div>
+      <button style={{margin: '10px'}} onClick={handleStartClick}>Solve Maze</button>
     </>
   )
 }
